ae434ecf33bd2a771b0a867235c9a8e7b37d53ae
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2018-02-18 19:56:15 -0400

    set calculate button listener to show the LoanDialogFragment for request Loan request information

--- app/src/main/java/beanstage/com/lender/LoanActivityFragment.java
+++ app/src/main/java/beanstage/com/lender/LoanActivityFragment.java
@@ -2,14 +2,16 @@ package beanstage.com.lender;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class LoanActivityFragment extends Fragment {
+public class LoanActivityFragment extends Fragment implements View.OnClickListener, LoanDialog.InfoDialogListener {
 
     public LoanActivityFragment() {
     }
@@ -17,6 +19,36 @@ public class LoanActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_loan, container, false);
+        View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_loan, container, false);
+
+        view.findViewById(R.id.calculate).setOnClickListener(this);
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.calculate:{
+                showLoanDialog();
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
+
+    private void showLoanDialog(){
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        LoanDialog loanDialog = new LoanDialog();
+        loanDialog.setTargetFragment(this, 300);
+        loanDialog.show(fm, "Loan Details");
+    }
+
+    @Override
+    public void onFinishEditInfo(Bundle info) {
+        Toast.makeText(getActivity(), "Amount: " + info.getInt("amount", 0), Toast.LENGTH_LONG).show();
     }
 }
