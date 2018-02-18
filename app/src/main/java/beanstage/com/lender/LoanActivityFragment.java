1bf47b06154c03391ea4bb04d4bfdca032e64736
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2018-02-17 23:09:08 -0400

    initial commit

---
+++ app/src/main/java/beanstage/com/lender/LoanActivityFragment.java
@@ -0,0 +1,22 @@
+package beanstage.com.lender;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class LoanActivityFragment extends Fragment {
+
+    public LoanActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_loan, container, false);
+    }
+}
