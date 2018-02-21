d2d7788f69c5f23926c173839a3baa6e5e7da9e3
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2018-02-21 19:14:24 -0400

    misc and other

--- app/src/main/java/beanstage/com/lender/LoanActivityFragment.java
+++ app/src/main/java/beanstage/com/lender/LoanActivityFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 public class LoanActivityFragment extends Fragment implements View.OnClickListener, LoanDialog.InfoDialogListener {
 
     public LoanActivityFragment() {
+
     }
 
     @Override
