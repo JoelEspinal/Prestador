a96b460e3932fa254f5e50d3bc3f36af540cb99a
JoelEspinal <j.espinalcarrasco@gmail.com>
2018-03-05 22:56:33 -0400

    calculate loan fee taking parameters

--- app/src/main/java/beanstage/com/lender/models/Loan.java
+++ app/src/main/java/beanstage/com/lender/models/Loan.java
@@ -15,32 +15,37 @@ public class Loan {
     public static final String IS_ANUAL = "IS_ANUAL";
 
     public double mAmount;
-    public double mRrate;
-    public int mTerm;
+    public double i;
+    public int n;
     public boolean mIsAnual;
 
-
-    public Loan(double amount, double rate, int n, boolean isAnual){
+    public Loan(double amount, double rate, int term, boolean isAnual){
         mAmount = amount;
-        mRrate = rate;
-        mTerm = n;
+        i = rate;
+        n = term;
         mIsAnual = isAnual;
     }
 
     public List<Fee> calculateFee(){
         List<Fee> feeList = new ArrayList<Fee>();
 
-       if(!mIsAnual){
-           mRrate = (double) mRrate/12;
+        double fee = 0.0;
+
+       if(mIsAnual){
+           i = (double) i/12;
        }
 
-        Fee fee = new Fee();
-        fee.fee = 100.0;
-        fee.amortization = 20.0;
-        fee.interest = 1.5;
-        fee.balance = 50;
+//      Calculate fees, french mode
+//      R = P [(i (1 + i)^n) / ((1 + i)^n – 1)]
+       fee = this.mAmount * ((i * Math.pow((1+ i), n))/(Math.pow((1 + i),  n) -  1));
+       fee = (double) Math.round(fee * 100) / 100;
+        for(int it = 0; it < n; it++){
+            Fee feeObj = new Fee();
+            feeObj.fee = fee;
 
-        feeList.add(fee);
+
+            feeList.add(feeObj);
+        }
 
         return feeList;
     }
