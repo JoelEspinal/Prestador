06a9659471fc85c1083f40e44c3c2703506fa52f
JoelEspinal <j.espinalcarrasco@gmail.com>
2018-03-09 19:28:09 -0400

    Add calculateFee method

--- app/src/main/java/beanstage/com/lender/models/Fee.java
+++ app/src/main/java/beanstage/com/lender/models/Fee.java
@@ -6,9 +6,21 @@ package beanstage.com.lender.models;
 
 public class Fee {
 
+    public int n;
     public double fee;
     public double amortization;
     public double interest;
     public double balance;
+
+
+
+    //      Calculate fees, french mode
+    //      R = P [(i (1 + i)^n) / ((1 + i)^n – 1)]
+    public static double calculateFee(double amount, double i, int n){
+        double fee = amount * ((i * Math.pow((1+ i), n))/(Math.pow((1 + i),  n) -  1));
+        fee = (double) Math.round(fee * 100) / 100;
+        return fee;
+    }
+
 
 }
