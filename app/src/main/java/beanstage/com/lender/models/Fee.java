b1350874b4bce256fb9bd059afafd00a69a5497a
JoelEspinal <j.espinalcarrasco@gmail.com>
2018-03-09 20:20:10 -0400

    Add calculateAmortization to corrent fee

--- app/src/main/java/beanstage/com/lender/models/Fee.java
+++ app/src/main/java/beanstage/com/lender/models/Fee.java
@@ -13,13 +13,40 @@ public class Fee {
     public double balance;
 
 
-
+    /**
+     *
+     * @param amount
+     * @param i
+     * @param n
+     * @return fee correspond to the n's loan fee
+     */
     //      Calculate fees, french mode
     //      R = P [(i (1 + i)^n) / ((1 + i)^n – 1)]
     public static double calculateFee(double amount, double i, int n){
-        double fee = amount * ((i * Math.pow((1+ i), n))/(Math.pow((1 + i),  n) -  1));
+        double fee = amount * ((i * Math.pow((1 + i), n))/(Math.pow((1 + i),  n) -  1));
         fee = (double) Math.round(fee * 100) / 100;
         return fee;
+    }
+
+
+    /**
+     *
+     * @param previousAmount
+     * @param i monthly interest
+     * @return the interest corresponding to current Fee
+     */
+    public static double calculateFeeInterest(double previousAmount, double i){
+        return (previousAmount * i);
+    }
+
+    /**
+     *
+     * @param currentFee
+     * @param interest
+     * @return Amortization for current fee
+     */
+    public static double calculateAmortization(double currentFee, double interest){
+        return (currentFee - interest);
     }
 
 
